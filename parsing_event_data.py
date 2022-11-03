7fea0879035af77a939f24876408474a18b2d9f7
Ilkka Ollakka <ilkka.ollakka@iki.fi>
2022-11-03 18:24:50 +0200

    parsing_event_data: add part B and populate fifth value
    simple pattern checking from output values based on hint string
    
    semver: +minor

--- parsing_event_data.py
+++ parsing_event_data.py
@@ -2,6 +2,7 @@
 
 import re
 import json
+import base64
 
 def read_events(line):
     # group match, first part is key, second part is value
@@ -21,6 +22,25 @@ def parse_events(filename):
                 whole_data = {**whole_data, **event}
         return json.dumps(whole_data)
 
+def populate_fifth_event(parsed_json):
+    #check hint
+    print('Outputing values to solve what fifth value could be')
+    print(f'\tprovided hint is "{base64.b64decode(parsed_json.get("hint"))}"')
+    # base64 decode the hint told to do xor with 0x17f
+    bitwise_xor_from_hint = int("0x17F", 16)
+    for value in ["one", "two", "three", "four"]:
+        # parse hex-string to int and do xor with hinted value
+        output = int(parsed_json.get(value), 16) ^ bitwise_xor_from_hint
+        #print output values for manual pattern calculating
+        print(f'\t{value} = {parsed_json.get(value)} xorred with 0x17F = {output} and in hex {hex(output)}')
+    # pattern seems to be nb nf n5 so fifth value would be 0x35 ^ 0x17F
+    # populate fifth value to dict
+    parsed_json['five'] = hex(int("0x35", 16) ^ bitwise_xor_from_hint)
+    return parsed_json
+
 if __name__ == "__main__":
     events = parse_events("event_data.txt")
     print(f'parsed json of events: {events}')
+    parsed_json = json.loads(events)
+    parsed_json = populate_fifth_event(parsed_json)
+    print(f'json with fifth event added: {parsed_json}')
